7074bd7e69fd153d043bc0c6ab49f74d515c6b46
Bing Wen Tan <btan@student.42singapore.sg>
2024-06-01 23:10:20 +0800

    Added mouse coordinates

--- srcs/events.c
+++ srcs/events.c
@@ -6,7 +6,7 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 16:04:00 by btan              #+#    #+#             */
-/*   Updated: 2024/06/01 14:18:54 by btan             ###   ########.fr       */
+/*   Updated: 2024/06/01 23:07:11 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,24 +39,25 @@ int	handle_keydown(int key, t_props *props)
 	return (0);
 }
 
-int	handle_mouse(int key, t_props *props)
+int	handle_mouse_click(int key, t_props *props)
 {
-	t_color *color;
-
-	printf("%d\n", key);
-	color = ft_calloc(1, sizeof(t_color));
-	color->red = 0;
-	color->green = 0;
-	color->blue = 255;
 	(void) props;
-//	props->pixel.color = rgb_to_dec(color);
-	draw_background(props);
-	free(color);
+	printf("%d\n", key);
 	return (0);
 }
+
+int	handle_mouse_coord(int x, int y, void *mlx_ptr, void *window)
+{
+	printf("x: %d, y: %d\n", x, y);
+	//if (key == 65307)
+	//	return (mlx_destroy_window(mlx_ptr, window));
+	return (0);
+}
+
 void	handle_events(t_props *props)
 {
 	mlx_hook(props->window, 2, 1L << 0, handle_keydown, props);
 	mlx_hook(props->window, 17, 0L, handle_close, props);
-	mlx_mouse_hook(props->window, handle_mouse, props);
+	mlx_mouse_hook(props->window, handle_mouse_click, props);
+	mlx_hook((*props).window, 6, 1L << 6, handle_mouse_coord, &props);
 }
