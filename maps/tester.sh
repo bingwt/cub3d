b6278f74c15f409769612aa6760c9267a86be4ad
xlow <lowxf7@gmail.com>
2024-07-13 07:59:54 +0800

    added tester script

---
+++ maps/tester.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TEST_FOLDER="./bad"
+PROGRAM="../cub3D"
+
+# Check if the program exists
+if [ ! -f "$PROGRAM" ]; then
+  echo "Error: Program $PROGRAM not found!"
+  exit 1
+fi
+
+for test_file in "$TEST_FOLDER"/*; do
+  if [ -f "$test_file" ]; then
+    echo "Running Valgrind on $test_file"
+    valgrind --leak-check=full "$PROGRAM" "$test_file"
+  else
+    echo "No test files found in $TEST_FOLDER"
+    exit 1
+  fi
+done
